cd1be8bf8f01afe7cf6145384db3a73c85b4394c
Микола Касянчук <148187561+balanceweb@users.noreply.github.com>
2025-10-29 11:50:22 +0200

    тернарний оператор

---
+++ script.js
@@ -0,0 +1,61 @@
+
+let age = 40;
+let prefix;
+
+if (age === 1) { prefix = 'year' }
+else { prefix = 'years' }
+
+console.log(`You are ${age} ${prefix} old`);
+
+let resultAge = age === 1 ? 'year' : 'years';
+console.log(age + ' ' + resultAge);
+
+
+let timeOfOder = 20;
+let smsClosed = 'Sorry the office is closed.';
+
+if (timeOfOder < 9 || timeOfOder > 18) {
+  console.log(smsClosed);
+}
+
+
+
+let skills = ['web', 'bots', 'seo'];
+
+let levelUp = skills.push("ppc", "txt");
+
+console.log(levelUp);
+console.log(skills);
+
+console.log(skills.join(' + '));
+console.log(skills.slice(0, 2));
+console.log(skills.splice(4, 1, "tgt"));
+console.log(skills.reverse());
+
+
+let balanceString = "Liberty";
+
+console.log(`Will be ${balanceString}`);
+
+let balanceBooling = true;
+
+console.log(`It is ${balanceBooling}`);
+
+let random;
+
+console.log(`random is ${random}`);
+
+let liberty_1 = Symbol('id');
+
+console.log(liberty_1);
+
+let balanceObject = {
+  profession: {
+    first: 'Front-end',
+    second: 'Bot developer'
+  },
+  credo: 'Be brave'
+
+}
+
+console.log(balanceObject);
